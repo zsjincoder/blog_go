e450a1b76394b5d8f48b85e03901270c49dae0c7
zhengsanjin <1932019050@qq.com>
2018-12-06 18:41:25 +0800

    first upDate

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"./pkg/setting"
+	"./routers"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	router := routers.InitRouter()
+	s := &http.Server{
+		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:router,
+		ReadTimeout:setting.ReadTimeout,
+		WriteTimeout:setting.WriteTimeout,
+		MaxHeaderBytes:1<<20,
+	}
+	s.ListenAndServe()
+}
---
+++ pkg/util/pagination.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"../setting"
+	"github.com/Unknwon/com"
+	"github.com/gin-gonic/gin"
+)
+
+func GetPage(c *gin.Context) int {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = setting.PageSize * (page - 1)
+	}
+	return result
+}
